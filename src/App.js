f280d9e0f9274e67cbe77f808f693b9fda75baa5
Joan Sweeney <joanms@eircom.net>
2020-07-04 17:29:10 +0100

    Enable deletion of characters on click

--- src/App.js
+++ src/App.js
@@ -12,9 +12,19 @@ class App extends Component {
     this.setState({userInput: event.target.value});
   }
 
+  deleteCharHandler = ( index ) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
+
   render() {
     const charList = this.state.userInput.split('').map((ch, index) => {
-      return <Char character={ch} key={index} />;
+      return <Char 
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)}/>;
     });
 
     return (
