0c968a6d7a4b1dea571bede6180422ad066fb556
Joan Sweeney <joanms@eircom.net>
2020-07-04 17:18:59 +0100

    Add Char component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './ValidationComponent/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
@@ -12,6 +13,10 @@ class App extends Component {
   }
 
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char character={ch} key={index} />;
+    });
+
     return (
       <div className="App">
         <ol>
@@ -26,6 +31,7 @@ class App extends Component {
         <input type="text" onChange={this.textInputHandler} value={this.state.userInput} />
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
